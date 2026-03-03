15e9295982facf71315d8c73fad04fcce744fb58
lixlim <s2alua.fu@gmail.com>
2026-03-03 09:50:30 +0900

    Update: change git-check to interactive branch switch

--- zsh/commands/git-check.zsh
+++ zsh/commands/git-check.zsh
@@ -1,6 +1,15 @@
-# Check git status in current directory
+# 깃 브랜치 스위칭
 git_check() {
-    git status
+    if ! (( $+commands[fzf] )); then
+        echo "❌ fzf가 없습니다: brew install fzf"
+        return 1
+    fi
+
+    local selected_branch
+    selected_branch="$(git branch | cut -c 3- | fzf --height=40% --reverse --prompt='Branch> ')" || return 0
+    [[ -n "$selected_branch" ]] || return 0
+
+    git switch "$selected_branch"
 }
 
 git_check "$@"
